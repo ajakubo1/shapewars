74c6f1a248307a055661f85216fea0fee31663b0
Adam Jakubowski <ajakubo1@gmail.com>
2015-08-15 23:29:38 +0200

    added object takeover

--- game/c.js
+++ game/c.js
@@ -27,6 +27,12 @@
             }
         ],
 
+        owned = [[[1, 1]], [1, 5]],
+
+        progressing = [[], []],
+
+        progressStatus = [],
+
         foreground = document.getElementById('f'),
         background = document.getElementById('b'),
         foreground_ctx = foreground.getContext('2d'),
@@ -68,22 +74,104 @@
         fore_minion_height = 30,
         fore_minion_width = 40,
 
+        progress_width = Math.floor(back_square_width / fore_minion_width),
+        progress_height = Math.floor(back_square_height / fore_minion_height),
+
         moved = false,
         moved_x = 0,
         moved_y = 0,
 
-        s_canvas = 'canvas';
+        s_canvas = 'canvas',
+
+        takeover_progress = 0;
+
+    function redrawBackground() {
+        background_ctx.clearRect(0, 0, width, height);
+        drawBackground();
+    }
+
+    function conquer(player, x, y) {
+        config[x][y] = current;
+        owned[player].push([x, y]);
+    }
+
+    function takeoverStep() {
+        if (progressStatus.length > 0) {
+            var i, j, took = false;
+            takeover_progress += 1;
+
+            if (takeover_progress === 20) {
+                took = true;
+                takeover_progress = 0;
+                for (i = 0; i < progress_width; i += 1) {
+                    for (j = 0; j < progress_height; j += 1) {
+                        if (progressStatus[0].progress[i][j] === 8) {
+                            progressStatus[0].progress[i][j] = current;
+                            took = false;
+                            break;
+                        }
+                    }
+
+                    if (!took) {
+                        break;
+                    }
+                }
+            }
+
+            if (took) {
+                took = progressStatus.shift();
+                conquer(current, took.x, took.y);
+                redrawBackground();
+            }
+        }
+    }
 
     function update(count) {
         while (count) {
             updateTime += tickLength;
-            //Update logic
+            //recount progress
+            takeoverStep();
             count -= 1;
         }
     }
 
+    function drawTakeover(player, x, y, sqx, sqy) {
+        background_ctx.drawImage(takeover_square[player], x + sqx * back_square_width + offset_x - current_x,
+                                 y + sqy * back_square_height + offset_y - current_y);
+    }
+
+    function drawProgress(x, y, progress) {
+        var i, j;
+        for (i = 0; i < progress_width; i += 1) {
+            for (j = 0; j < progress_height; j += 1) {
+                if (progress[i][j] !== 8) {
+                    drawTakeover(progress[i][j], i * fore_minion_width, j * fore_minion_height, x, y);
+                }
+            }
+        }
+    }
+
     function render() {
-        //Redraw everything
+        var i;
+        //clean frontend
+        foreground_ctx.clearRect(0, 0, width, height);
+        //Redraw progress
+        for (i = 0; i < progressStatus.length; i += 1) {
+            drawProgress(progressStatus[i].x, progressStatus[i].y, progressStatus[i].progress);
+        }
+        //Redraw minions
+    }
+
+    function inRange(player, x, y) {
+        var i, toReturn = false, sqr;
+        for (i = 0; i < owned[player].length; i += 1) {
+            sqr = owned[player][i];
+            if ((x === sqr[0] - 1 && y === sqr[1]) || (x === sqr[0] + 1 && y === sqr[1]) ||
+                    (x === sqr[0] && y === sqr[1] - 1) || (x === sqr[0] && y === sqr[1] + 1)) {
+                toReturn = true;
+            }
+        }
+        return toReturn;
     }
 
     function frame(frameTime) {
@@ -115,11 +203,6 @@
         }
     }
 
-    function redrawBackground() {
-        background_ctx.clearRect(0, 0, width, height);
-        drawBackground();
-    }
-
     function drawMinion(x, y, sqx, sqy) {
         foreground_ctx.drawImage(fore_minion, x + sqx * back_square_width + offset_x, y + sqy * back_square_height + offset_y);
     }
@@ -133,10 +216,6 @@
         context.stroke();
         context.fillStyle = fill;
         return context;
-    }
-
-    function drawTakeover(player, x, y, sqx, sqy) {
-        background_ctx.drawImage(takeover_square[player], x + sqx * back_square_width + offset_x, y + sqy * back_square_height + offset_y);
     }
 
     //Generation functions
@@ -234,34 +313,36 @@
         }
     }
 
+    function guardBorders() {
+        if (current_y < limit_y_top) {
+            current_y = limit_y_top;
+        } else if (current_y > y_limit) {
+            current_y = y_limit;
+        }
+        if (current_x < limit_x_left) {
+            current_x = limit_x_left;
+        } else if (current_x > x_limit) {
+            current_x = x_limit;
+        }
+    }
+
     function checkKey(e) {
         e = e || window.event;
-        var code = e.keyCode;
+        var code = e.keyCode,
+            moved = false;
 
         if (code === 38 || code === 87) {
             current_y -= step_y;
-            if (current_y < limit_y_top) {
-                current_y = limit_y_top;
-            }
-            redrawBackground();
+            moved = true;
         } else if (code === 40 || code === 83) {
             current_y += step_y;
-            if (current_y > y_limit) {
-                current_y = y_limit;
-            }
-            redrawBackground();
+            moved = true;
         } else if (code === 37 || code === 65) {
             current_x -= step_x;
-            if (current_x < limit_x_left) {
-                current_x = limit_x_left;
-            }
-            redrawBackground();
+            moved = true;
         } else if (code === 39 || code === 68) {
             current_x += step_x;
-            if (current_x > x_limit) {
-                current_x = x_limit;
-            }
-            redrawBackground();
+            moved = true;
         } else if (code === 107) {
             if (zoom > zoom_limit_in) {
                 zoom -= 1;
@@ -287,45 +368,15 @@
                 redrawBackground();
             }
         }
-    }
 
-    function mousedownListener(e) {
-        //console.info(e.pageX, e.pageY, "|", e.offsetX, e.offsetY, "|", e.screenX, e.screenY, "|", e.clientX, e.clientY);
-        foreground.addEventListener('mousemove', mousemoveListener);
-        foreground.addEventListener('mouseout', mouseoutListener);
-    }
-
-    function mouseupListener(e) {
-        foreground.removeEventListener('mousemove', mousemoveListener);
-        foreground.removeEventListener('mouseout', mouseoutListener);
-        if (moved === false) {
-            var x = e.offsetX + current_x - offset_x,
-                y = e.offsetY + current_y - offset_y;
-            if (x > 0 && y > 0 && x <= back_square_width * x_max && y <= back_square_height * y_max) {
-                x = Math.floor(x / back_square_width);
-                y = Math.floor(y / back_square_height);
-                if (config[x][y] !== 8) {
-                    //TODO: change this to give an order to squares
-                    config[x][y] = current;
-                    redrawBackground();
-                }
-            }
-        } else {
-            moved = false;
+        if (moved) {
+            guardBorders();
+            redrawBackground();
         }
     }
 
-    function touchstartListener(e) {
-        foreground.addEventListener('touchmove', touchmoveListener);
-    }
-
-    function touchendListener(e) {
-        foreground.removeEventListener('touchmove', touchmoveListener);
-        moved = false;
-    }
-
     function moveScreen(x, y) {
-        if(!moved) {
+        if (!moved) {
             moved = true;
             moved_x = x;
             moved_y = y;
@@ -334,23 +385,8 @@
         current_y -= y - moved_y;
         moved_x = x;
         moved_y = y;
-        if (current_y < limit_y_top) {
-            current_y = limit_y_top;
-        } else if (current_y > y_limit) {
-            current_y = y_limit;
-        }
-        if (current_x < limit_x_left) {
-            current_x = limit_x_left;
-        } else if (current_x > x_limit) {
-            current_x = x_limit;
-        }
+        guardBorders();
         redrawBackground();
-    }
-
-    function touchmoveListener(e) {
-        e.preventDefault();
-        var touch = e.touches[0];
-        moveScreen(touch.screenX, touch.screenY);
     }
 
     function mousemoveListener(e) {
@@ -362,10 +398,74 @@
         foreground.removeEventListener('mouseout', mouseoutListener);
     }
 
-     function mobileAndTabletcheck() {
-      var check = false;
-      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
-      return check;
+    function mousedownListener(e) {
+        //console.info(e.pageX, e.pageY, "|", e.offsetX, e.offsetY, "|", e.screenX, e.screenY, "|", e.clientX, e.clientY);
+        foreground.addEventListener('mousemove', mousemoveListener);
+        foreground.addEventListener('mouseout', mouseoutListener);
+    }
+
+    function progressTable(x, y) {
+        var i, j, toReturn = [];
+        for (i = 0; i < progress_width; i += 1) {
+            toReturn.push([]);
+            for (j = 0; j < progress_height; j += 1) {
+                toReturn[i].push(8);
+            }
+        }
+        return {
+            "x": x,
+            "y": y,
+            "progress": toReturn
+        };
+    }
+
+    function mouseupListener(e) {
+        foreground.removeEventListener('mousemove', mousemoveListener);
+        foreground.removeEventListener('mouseout', mouseoutListener);
+        if (moved === false) {
+            var x = e.offsetX + current_x - offset_x,
+                y = e.offsetY + current_y - offset_y;
+            if (x > 0 && y > 0 && x <= back_square_width * x_max && y <= back_square_height * y_max) {
+                x = Math.floor(x / back_square_width);
+                y = Math.floor(y / back_square_height);
+                if (config[x][y] !== 8 && inRange(current, x, y)) {
+                    //TODO: change this to give an order to squares
+                    progressing[current].push([0, x, y]);
+                    progressStatus.push(progressTable(x, y));
+                }
+            }
+        } else {
+            moved = false;
+        }
+    }
+
+    function touchmoveListener(e) {
+        e.preventDefault();
+        var touch = e.touches[0];
+        moveScreen(touch.screenX, touch.screenY);
+    }
+
+    function touchstartListener(e) {
+        foreground.addEventListener('touchmove', touchmoveListener);
+    }
+
+    function touchendListener(e) {
+        foreground.removeEventListener('touchmove', touchmoveListener);
+        moved = false;
+    }
+
+    function mobileAndTabletcheck() {
+        var check = false;
+        if (navigator.userAgent.match(/Android/i)
+                || navigator.userAgent.match(/webOS/i)
+                || navigator.userAgent.match(/iPhone/i)
+                || navigator.userAgent.match(/iPad/i)
+                || navigator.userAgent.match(/iPod/i)
+                || navigator.userAgent.match(/BlackBerry/i)
+                || navigator.userAgent.match(/Windows Phone/i)) {
+            check = true;
+        }
+        return check;
     }
 
     function startGame() {
@@ -387,13 +487,13 @@
         recountLimits();
         scaleToFit();
         window.addEventListener('resize', scaleToFit);
-        window.addEventListener('keydown', checkKey);
 
         if (mobileAndTabletcheck()) {
             foreground.addEventListener('touchstart', touchstartListener);
             foreground.addEventListener('touchend', touchendListener);
             foreground.addEventListener('mouseup', mouseupListener);
         } else {
+            window.addEventListener('keydown', checkKey);
             foreground.addEventListener('mousedown', mousedownListener);
             foreground.addEventListener('mouseup', mouseupListener);
         }
@@ -401,7 +501,7 @@
 
         updateTime = window.performance.now();
 
-        //window.requestAnimationFrame(frame);
+        window.requestAnimationFrame(frame);
     }
 
     startGame();
