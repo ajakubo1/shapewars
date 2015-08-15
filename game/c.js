4184fcf3b4e14aba8877c7fe4f448d12c034981b
Adam Jakubowski <ajakubo1@gmail.com>
2015-08-15 19:19:03 +0200

    Added click listeners

--- game/c.js
+++ game/c.js
@@ -1,11 +1,37 @@
 
 (function (document, window) {
     "use strict";
-    var config = [[0, 1, 0, 0, 1], [1, 1, 1, 1, 1], [0, 1, 0, 0, 1], [1, 1, 1, 1, 0], [1, 0, 1, 1, 1], [1, 0, 1, 1, 1]],
+    var config = [
+        [8, 9, 8, 8, 1],
+        [9, 0, 9, 9, 9],
+        [8, 9, 8, 8, 9],
+        [9, 9, 9, 9, 8],
+        [9, 8, 9, 9, 9],
+        [9, 8, 9, 9, 9]],
+        /*
+            types:
+                0 - current Player
+                1 - enemy Human
+                2 - enemy AI
+        */
+        players = [
+            {
+                "name": "claim",
+                "color": "blue",
+                "type": 0
+            },
+            {
+                "name": "pc",
+                "color": "green",
+                "type": 2
+            }
+        ],
+
         foreground = document.getElementById('f'),
         background = document.getElementById('b'),
         foreground_ctx = foreground.getContext('2d'),
         background_ctx = background.getContext('2d'),
+        current,
         width = 680,
         height = 480,
         x_max,
@@ -35,10 +61,11 @@
         limit_x_right,
         limit_y_top,
         limit_y_bottom,
-        back_square,
+        back_square = [],
         //Foreground settings
         fore_minion,
-        fore_minion_size = 32,
+        fore_minion_height = 30,
+        fore_minion_width = 40,
 
         s_canvas = 'canvas';
 
@@ -63,12 +90,12 @@
         }
     }
 
-    function drawBackgroundSquare(x, y) {
+    function drawBackgroundSquare(x, y, type) {
         var whole_x = x * back_square_width + offset_x - current_x,
             whole_y = y * back_square_height + offset_y - current_y;
 
         if (whole_x >= limit_x_left && whole_x <= limit_x_right && whole_y >= limit_y_top  && whole_y <= limit_y_bottom) {
-            background_ctx.drawImage(back_square, whole_x, whole_y);
+            background_ctx.drawImage(back_square[type], whole_x, whole_y);
         }
     }
 
@@ -76,8 +103,8 @@
         var i, j;
         for (i = 0; i < x_max; i += 1) {
             for (j = 0; j < y_max; j += 1) {
-                if (config[i][j] === 1) {
-                    drawBackgroundSquare(i, j);
+                if (config[i][j] !== 8) {
+                    drawBackgroundSquare(i, j, config[i][j]);
                 }
             }
         }
@@ -89,7 +116,7 @@
     }
 
     function drawMinion(x, y, sqx, sqy) {
-        background_ctx.drawImage(fore_minion, x + sqx * back_square_width, y + sqy * back_square_height);
+        foreground_ctx.drawImage(fore_minion, x + sqx * back_square_width + offset_x, y + sqy * back_square_height + offset_y);
     }
 
     function drawRect(context, x, y, width, height, stroke, fill, shadow) {
@@ -104,29 +131,49 @@
     }
 
     //Generation functions
-    function generateBackgroundSquare() {
-        back_square = document.createElement(s_canvas);
-        back_square.width = back_square_width;
-        back_square.height = back_square_height;
-        var context = back_square.getContext('2d');
-        context = drawRect(context, 2, 2, back_square_width - 4, back_square_height - 4, "Orange", "Gold");
+    function generateBackgroundSquare(fill, border) {
+        var square = document.createElement(s_canvas),
+            context;
+        square.width = back_square_width;
+        square.height = back_square_height;
+        context = square.getContext('2d');
+        //context = drawRect(context, 2, 2, back_square_width - 4, back_square_height - 4, border, fill);
+        context.beginPath();
+        context.rect(2, 2, width, height);
+        for(var i = 0 ; i < 4 ; i++) {
+            for(var j = 0 ; j < 5 ; j++) {
+                context.rect(i * fore_minion_width, j * fore_minion_width, fore_minion_width, fore_minion_height);
+            }
+        }
+
+        context.closePath();
+        context.lineWidth = 2;
+        context.strokeStyle = border;
+        context.stroke();
+        context.fillStyle = fill;
         context.fill();
+        return square;
     }
 
     function generateMinion() {
         fore_minion = document.createElement(s_canvas);
-        fore_minion.width = fore_minion_size;
-        fore_minion.height = fore_minion_size;
+        fore_minion.width = fore_minion_width;
+        fore_minion.height = fore_minion_height;
         var context = fore_minion.getContext('2d');
-        context = drawRect(context, fore_minion_size / 4, fore_minion_size / 4, fore_minion_size / 2, fore_minion_size / 2, "blue", "blue");
-        context.shadowBlur = 5;
-        context.shadowColor = "blue";
-        context.globalAlpha = 0.2;
+        context = drawRect(context, fore_minion_width / 4, fore_minion_height / 4, fore_minion_width / 2, fore_minion_height / 2, "white", "blue");
+        context.shadowBlur = 10;
+        context.shadowColor = "white";
+        context.globalAlpha = 1;
         context.fill();
     }
 
     function generate() {
-        generateBackgroundSquare();
+        var i;
+        back_square = [];
+        back_square[9] = generateBackgroundSquare("Orange", "Gold");
+        for (i = 0; i < players.length; i += 1) {
+            back_square[i] = generateBackgroundSquare(players[i].color, "Gold");
+        }
         generateMinion();
     }
 
@@ -234,7 +281,24 @@
         }
     }
 
+    function clickListener(e) {
+        //console.info(e.clientX, e.clientY, "|", e.pageX, e.pageY, "|", e.layerX, e.layerY, "|", e.offsetX, e.offsetY, "|", e.x, e.y);
+        var x = e.offsetX + current_x - offset_x,
+            y = e.offsetY + current_y - offset_y;
+
+        if(x > 0 && y > 0 && x <= back_square_width * x_max && y <= back_square_height * y_max) {
+            x = Math.floor(x / back_square_width);
+            y = Math.floor(y / back_square_height);
+            if(config[x][y] !== 8) {
+                //TODO: change this to give an order to squares
+                config[x][y] = current;
+                redrawBackground();
+            }
+        }
+    }
+
     function startGame() {
+        var i;
         foreground.width = width;
         foreground.height = height;
         background.width = width;
@@ -242,12 +306,43 @@
         x_max = config.length;
         y_max = config[0].length;
 
+        for (i = 0; i < players.length; i += 1) {
+            if(players[i].type === 0) {
+                current = i;
+                break;
+            }
+        }
+
         recountLimits();
         scaleToFit();
         window.addEventListener('resize', scaleToFit);
         window.addEventListener('keydown', checkKey);
+        foreground.addEventListener('click', clickListener);
         drawBackground();
-        drawMinion(20, 20, 1, 1);
+        drawMinion(0, 0, 1, 1);
+        drawMinion(40, 0, 1, 1);
+        drawMinion(80, 0, 1, 1);
+        drawMinion(120, 0, 1, 1);
+
+        drawMinion(0, 30, 1, 1);
+        drawMinion(40, 30, 1, 1);
+        drawMinion(80, 30, 1, 1);
+        drawMinion(120, 30, 1, 1);
+
+        drawMinion(0, 60, 1, 1);
+        drawMinion(40, 60, 1, 1);
+        drawMinion(80, 60, 1, 1);
+        drawMinion(120, 60, 1, 1);
+
+        drawMinion(0, 90, 1, 1);
+        drawMinion(40, 90, 1, 1);
+        drawMinion(80, 90, 1, 1);
+        drawMinion(120, 90, 1, 1);
+
+        drawMinion(0, 120, 1, 1);
+        drawMinion(40, 120, 1, 1);
+        drawMinion(80, 120, 1, 1);
+        drawMinion(120, 120, 1, 1);
 
         updateTime = window.performance.now();
 
