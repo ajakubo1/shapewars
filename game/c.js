4095d1a02afa5d1761ca6bfe115dc6e5c9df29c3
Adam Jakubowski <ajakubo1@gmail.com>
2015-08-15 22:07:34 +0200

    Added drag&drop movement

--- game/c.js
+++ game/c.js
@@ -62,10 +62,15 @@
         limit_y_top,
         limit_y_bottom,
         back_square = [],
+        takeover_square = [],
         //Foreground settings
         fore_minion,
         fore_minion_height = 30,
         fore_minion_width = 40,
+
+        moved = false,
+        moved_x = 0,
+        moved_y = 0,
 
         s_canvas = 'canvas';
 
@@ -130,6 +135,10 @@
         return context;
     }
 
+    function drawTakeover(player, x, y, sqx, sqy) {
+        background_ctx.drawImage(takeover_square[player], x + sqx * back_square_width + offset_x, y + sqy * back_square_height + offset_y);
+    }
+
     //Generation functions
     function generateBackgroundSquare(fill, border) {
         var square = document.createElement(s_canvas),
@@ -137,20 +146,18 @@
         square.width = back_square_width;
         square.height = back_square_height;
         context = square.getContext('2d');
-        //context = drawRect(context, 2, 2, back_square_width - 4, back_square_height - 4, border, fill);
-        context.beginPath();
-        context.rect(2, 2, width, height);
-        for(var i = 0 ; i < 4 ; i++) {
-            for(var j = 0 ; j < 5 ; j++) {
-                context.rect(i * fore_minion_width, j * fore_minion_width, fore_minion_width, fore_minion_height);
-            }
-        }
+        context = drawRect(context, 2, 2, back_square_width - 4, back_square_height - 4, border, fill);
+        context.fill();
+        return square;
+    }
 
-        context.closePath();
-        context.lineWidth = 2;
-        context.strokeStyle = border;
-        context.stroke();
-        context.fillStyle = fill;
+    function generateTakeoverSquare(fill, border) {
+        var square = document.createElement(s_canvas),
+            context;
+        square.width = fore_minion_width;
+        square.height = fore_minion_height;
+        context = square.getContext('2d');
+        context = drawRect(context, 0, 0, fore_minion_width, fore_minion_height, border, "green");
         context.fill();
         return square;
     }
@@ -173,6 +180,7 @@
         back_square[9] = generateBackgroundSquare("Orange", "Gold");
         for (i = 0; i < players.length; i += 1) {
             back_square[i] = generateBackgroundSquare(players[i].color, "Gold");
+            takeover_square[i] = generateTakeoverSquare(players[i].color, "Gold");
         }
         generateMinion();
     }
@@ -281,20 +289,83 @@
         }
     }
 
-    function clickListener(e) {
-        //console.info(e.clientX, e.clientY, "|", e.pageX, e.pageY, "|", e.layerX, e.layerY, "|", e.offsetX, e.offsetY, "|", e.x, e.y);
-        var x = e.offsetX + current_x - offset_x,
-            y = e.offsetY + current_y - offset_y;
+    function mousedownListener(e) {
+        //console.info(e.pageX, e.pageY, "|", e.offsetX, e.offsetY, "|", e.screenX, e.screenY, "|", e.clientX, e.clientY);
+        foreground.addEventListener('mousemove', mousemoveListener);
+        foreground.addEventListener('mouseout', mouseoutListener);
+    }
 
-        if(x > 0 && y > 0 && x <= back_square_width * x_max && y <= back_square_height * y_max) {
-            x = Math.floor(x / back_square_width);
-            y = Math.floor(y / back_square_height);
-            if(config[x][y] !== 8) {
-                //TODO: change this to give an order to squares
-                config[x][y] = current;
-                redrawBackground();
+    function mouseupListener(e) {
+        foreground.removeEventListener('mousemove', mousemoveListener);
+        foreground.removeEventListener('mouseout', mouseoutListener);
+        if (moved === false) {
+            var x = e.offsetX + current_x - offset_x,
+                y = e.offsetY + current_y - offset_y;
+            if (x > 0 && y > 0 && x <= back_square_width * x_max && y <= back_square_height * y_max) {
+                x = Math.floor(x / back_square_width);
+                y = Math.floor(y / back_square_height);
+                if (config[x][y] !== 8) {
+                    //TODO: change this to give an order to squares
+                    config[x][y] = current;
+                    redrawBackground();
+                }
             }
+        } else {
+            moved = false;
         }
+    }
+
+    function touchstartListener(e) {
+        foreground.addEventListener('touchmove', touchmoveListener);
+    }
+
+    function touchendListener(e) {
+        foreground.removeEventListener('touchmove', touchmoveListener);
+        moved = false;
+    }
+
+    function moveScreen(x, y) {
+        if(!moved) {
+            moved = true;
+            moved_x = x;
+            moved_y = y;
+        }
+        current_x -= x - moved_x;
+        current_y -= y - moved_y;
+        moved_x = x;
+        moved_y = y;
+        if (current_y < limit_y_top) {
+            current_y = limit_y_top;
+        } else if (current_y > y_limit) {
+            current_y = y_limit;
+        }
+        if (current_x < limit_x_left) {
+            current_x = limit_x_left;
+        } else if (current_x > x_limit) {
+            current_x = x_limit;
+        }
+        redrawBackground();
+    }
+
+    function touchmoveListener(e) {
+        e.preventDefault();
+        var touch = e.touches[0];
+        moveScreen(touch.screenX, touch.screenY);
+    }
+
+    function mousemoveListener(e) {
+        moveScreen(e.offsetX, e.offsetY);
+    }
+
+    function mouseoutListener(e) {
+        foreground.removeEventListener('mousemove', mousemoveListener);
+        foreground.removeEventListener('mouseout', mouseoutListener);
+    }
+
+     function mobileAndTabletcheck() {
+      var check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+      return check;
     }
 
     function startGame() {
@@ -307,7 +378,7 @@
         y_max = config[0].length;
 
         for (i = 0; i < players.length; i += 1) {
-            if(players[i].type === 0) {
+            if (players[i].type === 0) {
                 current = i;
                 break;
             }
@@ -317,32 +388,16 @@
         scaleToFit();
         window.addEventListener('resize', scaleToFit);
         window.addEventListener('keydown', checkKey);
-        foreground.addEventListener('click', clickListener);
+
+        if (mobileAndTabletcheck()) {
+            foreground.addEventListener('touchstart', touchstartListener);
+            foreground.addEventListener('touchend', touchendListener);
+            foreground.addEventListener('mouseup', mouseupListener);
+        } else {
+            foreground.addEventListener('mousedown', mousedownListener);
+            foreground.addEventListener('mouseup', mouseupListener);
+        }
         drawBackground();
-        drawMinion(0, 0, 1, 1);
-        drawMinion(40, 0, 1, 1);
-        drawMinion(80, 0, 1, 1);
-        drawMinion(120, 0, 1, 1);
-
-        drawMinion(0, 30, 1, 1);
-        drawMinion(40, 30, 1, 1);
-        drawMinion(80, 30, 1, 1);
-        drawMinion(120, 30, 1, 1);
-
-        drawMinion(0, 60, 1, 1);
-        drawMinion(40, 60, 1, 1);
-        drawMinion(80, 60, 1, 1);
-        drawMinion(120, 60, 1, 1);
-
-        drawMinion(0, 90, 1, 1);
-        drawMinion(40, 90, 1, 1);
-        drawMinion(80, 90, 1, 1);
-        drawMinion(120, 90, 1, 1);
-
-        drawMinion(0, 120, 1, 1);
-        drawMinion(40, 120, 1, 1);
-        drawMinion(80, 120, 1, 1);
-        drawMinion(120, 120, 1, 1);
 
         updateTime = window.performance.now();
 
