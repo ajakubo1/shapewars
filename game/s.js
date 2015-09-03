f695c39cc38af66a628fad99e0d2a53fc957deb1
Adam Jakubowski <ajakubo1@gmail.com>
2015-09-04 01:02:06 +0200

    added some server-side scripts

--- game/s.js
+++ game/s.js
@@ -1,14 +1,66 @@
 var io = require('sandbox-io');
 
-io.on('connection', function(socket) {
-  // See the generated log in the server console:
-  log.debug('New connection', socket.id);
-  // Send a message to this player:
-  socket.emit('srv-msg', { message: 'Welcome!' });
-  // Link a receiveClientMessage reference to this socket
-  // and add it as a listener for 'cli-msg' events:
-  socket.on('cli-msg', receiveClientMessage.bind(socket));
+var room_id = 1;
+var player_id = 1;
+
+var lobby2 = [];
+var lobby4 = [];
+
+var rooms = {};
+var players = {};
+
+io.on('connection', function (socket) {
+    // See the generated log in the server console:
+    log.debug('New connection', socket.id);
+    socket.on('register', registration.bind(socket));
 });
+
+function registration(data) {
+    if(data.mode === 0) {
+        //Single player, let the party started :)
+        //Generate the map and move on.
+    } else {
+        var i, id;
+        //Multiplayer
+        data.socket = this;
+        if (data.players === 2) {
+            lobby2.push(data);
+            if(lobby2.length === 2) {
+                //All in, let the party started :)
+                //Generate the map and move on
+                id = 'room' + room_id;
+                room_id += 1;
+                rooms[id] = {
+                    'status': 0,
+                    'players': []
+                }
+
+                for (i = 0; i < 2; i += 1) {
+                    log.debug('sending request to: ' + lobby2[i].name);
+                    lobby2[i].socket.emit('start');
+                }
+
+                lobby2 = [];
+            }
+        } else if (data.players === 4) {
+            lobby4.push(data);
+            if(lobby2.length === 4) {
+                //All in, let the party started :)
+                //Generate the map and move on
+                for (i = 0; i < 4; i += 1) {
+                    log.debug('sending request to: ' + lobby2[i].name);
+                    lobby4[i].socket.emit('start');
+                }
+
+                lobby4 = [];
+            }
+        }
+    }
+}
+
+function start(data) {
+
+}
 
 function receiveClientMessage(data) {
   if (data == 'Hello') {
